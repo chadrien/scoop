1daef34aadd766c23026eedbba7cc74c559f5563
chadrien <hadrien@decuzey.fr>
2019-12-12 23:26:35 +0200

    Update MTGO

--- mtgo_install.ps1
+++ mtgo_install.ps1
@@ -2,16 +2,16 @@ param($dir)
 
 start $dir\setup.exe -wait
 
-:: ClickOnce installer will start MTGO right away, stop it right away too
+# ClickOnce installer will start MTGO right away, let's stop it right away too
 
-:: wait until MTGO has started
+# wait until MTGO has started
 $started = $false
 Do {
     $status = Get-Process notepad -ErrorAction SilentlyContinue
-    If (!($status)) { Write-Host 'Waiting for process to start' ; Start-Sleep -Seconds 1 }
-    Else { Write-Host 'Process has started' ; $started = $true }
+    If (!($status)) { Start-Sleep -Seconds 1 }
+    Else { $started = $true }
 }
 Until ( $started )
 
-:: then close it
+# then close it
 Stop-Process -Name mtgo
