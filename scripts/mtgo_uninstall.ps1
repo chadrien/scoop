02f3b3fef00a353d8ff32049764dc8a8153945b7
chadrien <hadrien@decuzey.fr>
2019-12-12 23:59:28 +0200

    Update mtgo

--- scripts/mtgo_uninstall.ps1
+++
@@ -1,2 +0,0 @@
-$UninstallString = Get-ChildItem HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall | foreach-object {Get-ItemProperty $_.PsPath} | ? { $_.displayname -match "Magic The Gathering Online" } | select uninstallstring
-cmd /c $UninstallString.UninstallString
